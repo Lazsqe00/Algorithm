c8f587cb4e5318e4904cbf9cfd1b19649aaf9117
Lazsqe00 <kidieu000@gmail.com>
2025-07-02 17:05:02 +0700

    Create DSU.cpp

---
+++ DSU.cpp
@@ -0,0 +1,32 @@
+int par[MAX], sz[MAX];
+int T, N, M, cnt;
+vector<int> adj[MAX];
+
+
+int find(int v){
+	if(v == par[v]) return v;
+	return par[v] = find(par[v]);
+}
+// chú ý rất hay quên init_DSU
+void init_DSU(){
+	FOR(i, 1, N){
+		par[i] = i;
+	}
+}
+
+bool Union(int a, int b){
+	int u = find(a);
+	int v = find(b);
+	if(u == v) return false; 
+	else{
+		if(sz[u] >= sz[v]){
+			par[v] = u;
+			sz[u] += sz[v];
+		}
+		else{
+			par[u] = v;
+			sz[v] += sz[u];
+		}
+	}
+	return true;
+}
